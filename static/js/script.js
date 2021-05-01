98b738cdd4ddc2f34f3f0b7f85d4185ac3e97109
Carolina Cobo <ccobo.dev@gmail.com>
2021-05-01 13:58:18 +0100

    Form

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,15 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav({edge:"right"});
-    $('.collapsible').collapsible();
-    $('.tooltipped').tooltip();
-
+$(document).ready(function () {
+  $(".sidenav").sidenav({edge: "right"});
+  $(".collapsible").collapsible();
+  $(".tooltipped").tooltip();
+  $(".datepicker").datepicker({
+    // 
+      format: "dd mmmm, yyyy",
+      yearRange: 3,
+      showClearBtn: true,
+      // own object. Internationalization and localization
+      i18n: {
+          done: "Select"
+      }
   });
+});
