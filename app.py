8018235873b1929776ef594bdf41faeac2e0328d
Carolina Cobo <ccobo.dev@gmail.com>
2021-05-01 20:55:56 +0100

    Delete tasks from the DB

--- app.py
+++ app.py
@@ -145,6 +145,14 @@ def edit_task(task_id):
 
 
 
+@app.route("/delete_task/<task_id>")
+def delete_task(task_id):
+    mongo.db.tasks.remove({"_id": ObjectId(task_id)})
+    flash("Task Successfully Deleted")
+    return redirect(url_for("get_tasks"))
+
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
